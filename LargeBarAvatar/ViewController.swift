724f00e7c6266b9284da28ec2e051951a358c8de
Parshutkin Pavel <parshutkin.p@orionnet.ru>
2023-07-24 20:27:08 +0700

    Initial Commit

---
+++ LargeBarAvatar/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  RectangleGradient
+//
+//  Created by Pavel Parshutkin on 02.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var scrollView: UIScrollView = configureScrollView()
+    private lazy var avatarView: UIImageView = configureAvatarView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(scrollView)
+        self.title = "Avatar"
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+
+        self.setupConstraints()
+        
+        
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.addItemToLargeNavigationBar(itemView: avatarView)
+    }
+    
+    private func configureAvatarView() -> UIImageView {
+        let imageView = UIImageView()
+        
+        let image = UIImage(systemName: "person.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 28))
+        
+        imageView.image = image
+        imageView.tintColor = .gray
+        imageView.clipsToBounds = true
+        imageView.layer.zPosition = 0
+        return imageView
+    }
+    
+    private func configureScrollView() -> UIScrollView {
+        let view = UIScrollView()
+        view.alwaysBounceVertical = true
+        view.contentSize = .init(width: view.frame.width, height: 2000)
+     
+        return view
+    }
+    
+    
+    private func addItemToLargeNavigationBar(itemView: UIView) {
+        guard let navigationBar = self.navigationController?.navigationBar else { return }
+        guard let UINavigationBarLargeTitleView = NSClassFromString("_UINavigationBarLargeTitleView") else { return
+            
+        }
+        
+        navigationBar.subviews.forEach { subview in
+            
+                if subview.isKind(of: UINavigationBarLargeTitleView.self) {
+    
+                    subview.addSubview(itemView)
+        
+                    itemView.translatesAutoresizingMaskIntoConstraints = false
+                    
+                    NSLayoutConstraint.activate([
+                        itemView.bottomAnchor.constraint(
+                            equalTo: subview.bottomAnchor,
+                            constant: -12
+                        ),
+                        itemView.trailingAnchor.constraint(
+                            equalTo: subview.trailingAnchor,
+                            constant: -16
+                        )
+                    ])
+                }
+            }
+        
+
+    }
+    private func setupConstraints() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
